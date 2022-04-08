766b9fd8a6d4402f09ab4093480a744483225cf1
Vladislav <v.verokha@keep-calm.ru>
2022-04-08 21:24:19 +0300

    урок 4

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+    console.log('hello');
+    var x = '123';
+    var b = 123;
+
+    if (x == b) {
+        let a = '123';
+        let c = 123;
+    }
+
+    const type1 = '123';
+    const type2 = 123;
+
+    $('#button1').click(function () {
+        console.log('clicked');
+    });
+
+    $('.button2').click(function () {
+       console.log('clicked 2');
+    });
+
+    var a = {
+        name: 'vlad',
+        first_name : {
+
+        }
+    };
+
+});
